def8b97ab525bcfeb5db6d22dd6d5df4013b8111
MariyaVik <95871247+MariyaVik@users.noreply.github.com>
2022-05-22 17:09:39 +0500

    add ball

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from pygame import *
 
 class GameSprite(sprite.Sprite):
   # конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, speed):
         # Вызываем конструктор класса (Sprite):
         sprite.Sprite.__init__(self)
 
         # каждый спрайт должен хранить свойство image - изображение
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
+        self.speed = speed
 
         # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
@@ -40,9 +40,11 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 
 img_rocket = 'images/racket.png'
+img_ball = 'images/ball.png'
 
 rocket1 = Player(img_rocket, 10, 200, 50, 200, 5)
 rocket2 = Player(img_rocket, win_width - 60, 200, 50, 200, 5)
+ball = GameSprite(img_ball, 200, 200, 50, 50, 2)
 
 clock = time.Clock()
 FPS = 60
@@ -64,6 +66,7 @@ while run:
 
         rocket1.reset()
         rocket2.reset()
+        ball.reset()
 
         display.update()
         clock.tick(FPS)
